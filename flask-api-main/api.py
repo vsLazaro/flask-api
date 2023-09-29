243b4b1d3c55f99ba137096a2ad0c0f8e0fde67e
t-bsouza <thiagoborgesrs@hotmail.comm>
2023-09-28 22:29:10 -0300

    Esqueleto da API

--- flask-api-main/api.py
+++ flask-api-main/api.py
@@ -1,13 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
 
-# Configuração do banco de dados
-# Por exemplo, usar SQLite para desenvolvimento
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+# Configuração do banco de dados para MySQL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/dbname'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -25,18 +24,46 @@ class PlaneCrash(db.Model):
     cn_ln = db.Column(db.String)
     aboard = db.Column(db.String)
     fatalities = db.Column(db.String)
-    ground = db.Column(db.Integer)
-    summary = db.Column(db.Text)
+    ground = db.Column(db.String)
+    summary = db.Column(db.String)
 
 class PlaneCrashResource(Resource):
-    def get(self, crash_id):
-        crash = PlaneCrash.query.get_or_404(crash_id)
-        return {
+    def get(self, crash_id=None):
+        if crash_id:
+            crash = PlaneCrash.query.get_or_404(crash_id)
+            return jsonify({
+                'id': crash.id,
+                'date': crash.date,
+                'time': crash.time,
+                'location': crash.location,
+                'operator': crash.operator,
+                'flight-number': crash.flight_number,
+                'route': crash.route,
+                'ac_type': crash.ac_type,
+                'registration': crash.registration,
+                'cn_ln': crash.cn_ln,
+                'aboard': crash.aboard,
+                'fatalities': crash.fatalities,
+                'ground': crash.ground,
+                'summary': crash.summary
+            })
+        crashes = PlaneCrash.query.all()
+        return jsonify([{
             'id': crash.id,
             'date': crash.date,
             'time': crash.time,
-            # Adicionar as outras colunas
-        }
+            'location': crash.location,
+            'operator': crash.operator,
+            'flight-number': crash.flight_number,
+            'route': crash.route,
+            'ac_type': crash.ac_type,
+            'registration': crash.registration,
+            'cn_ln': crash.cn_ln,
+            'aboard': crash.aboard,
+            'fatalities': crash.fatalities,
+            'ground': crash.ground,
+            'summary': crash.summary
+        } for crash in crashes])
 
     def post(self):
         args = request.get_json()
@@ -45,7 +72,19 @@ class PlaneCrashResource(Resource):
         db.session.commit()
         return {'id': new_crash.id}, 201
 
-    # Implementar os métodos PUT e DELETE
+    def put(self, crash_id):
+        crash = PlaneCrash.query.get_or_404(crash_id)
+        args = request.get_json()
+        for key, value in args.items():
+            setattr(crash, key, value)
+        db.session.commit()
+        return {'message': f'Crash {crash_id} updated successfully.'}
+    
+    def delete(self, crash_id):
+        crash = PlaneCrash.query.get_or_404(crash_id)
+        db.session.delete(crash)
+        db.session.commit()
+        return {'message': f'Crash {crash_id} deleted successfully.'}, 204
 
 # Rota para o recurso
 api.add_resource(PlaneCrashResource, '/plane_crash', '/plane_crash/<int:crash_id>')
